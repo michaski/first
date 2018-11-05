c5fec0134847c3aefb9a7131ad23023c9a6f8dae
michaski <44462112+michaski@users.noreply.github.com>
2018-11-05 12:20:24 +0100

    Poprawiono. Teraz program zwraca poprawny wynik

--- plecak.cpp
+++ plecak.cpp
@@ -12,19 +12,19 @@ int main(int argc, char** argv)
 		cin >> CENA[i];
 	for(int i=1; i<=n; i++)
 		cin >> WAGA[i];
-		
+
 	for(int i=1; i<=n; i++)
 	{
 		for(int j=1; j<=WG; j++)
 		{
 			if(j >= WAGA[i])
-				TAB[i][j] = max( TAB[i-1][j], TAB[i-1][j-WAGA[i]]-CENA[i] );
+				TAB[i][j] = max( TAB[i-1][j], TAB[i-1][j-WAGA[i]]+CENA[i] );
 			else
 				TAB[i][j] = TAB[i-1][j];
 		}
-	}	
-	
+	}
+
 	cout << TAB[n][WG] << endl;
-	
+
 	return 0;
 }
